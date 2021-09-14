a90c20fec47b0553d82767a7011afe8cc262090d
Leandro Sousa <leandro@desencoder.com.br>
2021-09-14 14:48:00 -0300

    add details guide screen

--- app-me-guiae/app/src/screens/DetailsGuide/DetailsGuide.js
+++ app-me-guiae/app/src/screens/DetailsGuide/DetailsGuide.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 
 import ScrollContainer from "../../components/ScrollContainer";
-import { RoundPixel, widthDPI } from "../../utils/roundPixel";
-import { shadow } from "../../globalStyles/shadow";
-import { PALLET } from "../../globalStyles/palletColor";
+
+import { Ionicons } from "@expo/vector-icons";
+
+import { styles } from "./styles";
 
 const DetailsGuide = ({ route }) => {
-  console.warn(route);
+  // console.warn(route);
   return (
     <>
       <View style={styles.containerImage}>
@@ -26,13 +27,13 @@ const DetailsGuide = ({ route }) => {
           <View style={styles.card}>
             <Text style={styles.title}>Sobre {route.params.fullName}</Text>
             <Text style={styles.text}>{route.params.description}</Text>
-            <Text style={styles.title}>
+            <Text style={styles.text}>
               Especialidades: {route.params.specialties}
             </Text>
-            <Text style={styles.title}>
+            <Text style={styles.text}>
               Valor por pessoa: R${route.params.value}
             </Text>
-            <Text style={styles.title}>Destino: {route.params.routes[0]}</Text>
+            <Text style={styles.text}>Destino: {route.params.routes[0]}</Text>
             <TouchableOpacity
               style={styles.button}
               onPress={() => alert("Agendar...")}
@@ -40,67 +41,25 @@ const DetailsGuide = ({ route }) => {
               <Text style={styles.textButton}>Me Guiaê</Text>
             </TouchableOpacity>
           </View>
+
+          <View style={styles.card}>
+            <Text style={styles.title}>Conquistas</Text>
+            <Ionicons name={route.params.achievements[0]} size={24} />
+          </View>
+
+          <View style={styles.card}>
+            <Text style={styles.title}>
+              Pontos Turísticos do {route.params.fullName}
+            </Text>
+            <Text style={styles.title}>Destino:</Text>
+            {route.params.routes.map((item) => (
+              <Text style={styles.text}>{item}</Text>
+            ))}
+          </View>
         </View>
       </ScrollContainer>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: RoundPixel(20),
-    alignItems: "center",
-  },
-  card: {
-    width: widthDPI(90),
-    borderRadius: RoundPixel(16),
-    padding: RoundPixel(16),
-    top: RoundPixel(12),
-    backgroundColor: PALLET.light,
-    ...shadow.shadow,
-  },
-  containerImage: {
-    alignItems: "center",
-  },
-  cape: {
-    width: widthDPI(100),
-    height: RoundPixel(148),
-  },
-  thumbnail: {
-    width: RoundPixel(100),
-    height: RoundPixel(100),
-    borderRadius: RoundPixel(50),
-    position: "absolute",
-    top: RoundPixel(80),
-  },
-  title: {
-    fontWeight: "bold",
-    color: PALLET.secondColor,
-    fontSize: 14,
-  },
-  text: {
-    color: PALLET.fontThin,
-    fontSize: 14,
-  },
-  button: {
-    paddingLeft: RoundPixel(12),
-    paddingRight: RoundPixel(12),
-    height: RoundPixel(32),
-    width: RoundPixel(112),
-    right: RoundPixel(16),
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: RoundPixel(16),
-    backgroundColor: PALLET.primaryColor,
-    ...shadow.shadow,
-    position: "absolute",
-    bottom: RoundPixel(16),
-  },
-  textButton: {
-    color: PALLET.light,
-    fontWeight: "bold",
-    fontSize: 14,
-  },
-});
 
 export default DetailsGuide;
