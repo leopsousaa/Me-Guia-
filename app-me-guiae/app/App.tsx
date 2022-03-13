1517630fa4b9f0ff65d0b6842366608526842cfd
leopsousaa <leo.sousa3108@gmail.com>
2022-03-13 19:22:10 -0300

    resolve font errorr

--- app-me-guiae/app/App.tsx
+++ app-me-guiae/app/App.tsx
@@ -1,22 +1,35 @@
 import 'react-native-gesture-handler'
 import React from 'react'
+import { useFonts } from 'expo-font'
+
+import AppLoading from 'expo-app-loading'
+
 import { View, Text } from 'react-native'
-
-// import AppLoading from 'expo-app-loading'
-
-// import { fontsLoaded } from './font'
 
 import { NavigationContainer } from '@react-navigation/native'
 
 const App: React.FC = () => {
-  // if (!fontsLoaded) {
-  //   return <AppLoading />
-  // }
+  const [fontsLoaded] = useFonts({
+    'BebasNeue-Regular': require('./assets/fonts/BebasNeue-Regular.ttf'),
+    'Inter-Thin': require('./assets/fonts/Inter-Thin.ttf'),
+    'Inter-Light': require('./assets/fonts/Inter-Light.ttf'),
+    'Inter-ExtraLight': require('./assets/fonts/Inter-ExtraLight.ttf'),
+    'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+    'Inter-Medium': require('./assets/fonts/Inter-Medium.ttf'),
+    'Inter-SemiBold': require('./assets/fonts/Inter-SemiBold.ttf'),
+    'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+    'Inter-ExtraBold': require('./assets/fonts/Inter-ExtraBold.ttf'),
+    'Inter-Black': require('./assets/fonts/Inter-Black.ttf')
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
 
   return (
     <NavigationContainer>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>teste</Text>
+        <Text>DesenCoder...</Text>
       </View>
     </NavigationContainer>
   )
--- app-me-guiae/app/react-native.config.js
+++ app-me-guiae/app/react-native.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   project: {
     ios: {},
-    android: {},
+    android: {}
   },
-  assets: ['./App/assets/fonts'],
-};
+  assets: ['./App/assets/fonts']
+}
